dbe9c864c921f2abbd2df8bfce572b034fabbb62
Meenakshi19-cse <meenakshiharish2006@gmail.com>
2025-10-29 11:42:37 +0530

    my first commit

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [form, setForm] = useState({name:'', email:'', pass:'', confirm:'', otp:''});
+  const [users, setUsers] = useState([]);
+  
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+ 
+    if (form.pass !== form.confirm) {
+      alert('Password doent match');
+      return;
+    }
+
+    if (form.pass.length < 8) {
+      alert('Required Min 8 chars');
+      return;
+    }
+if(form.email != "@"){
+  alert("email format is wrong!!")
+  return;
+ }
+    const arr = [123, 234, 345];
+
+    if ("123" !== form.otp) {
+      console.log(typeof(form.otp));
+      alert('Not match otp')
+      return;
+    }
+
+    setUsers([...users, form]);
+    setForm({name:'', email:'', pass:'', confirm:'', otp:''})
+
+    alert('OTP Verified Successfully, now you have registerd')
+  }
+
+  return (
+    <>
+    <h1>Registration Form</h1>
+      <form>
+        <input type="text" placeholder='Enter username' value={form.name} onChange={(e) => setForm({...form, name: e.target.value})}/>
+        <input type="email" placeholder='Enter email' value={form.email} onChange={(e) => setForm({...form, email: e.target.value})}/>
+        <input type="text" placeholder='Enter password' value={form.pass} onChange={(e) => setForm({...form, pass: e.target.value})}/>
+        <input type="text" placeholder='Enter confirm password' value={form.confirm} onChange={(e) => setForm({...form, confirm: e.target.value})}/>
+        <input type="text" placeholder='enter otp from 123, 234, 345' value={form.otp} onChange={(e) => setForm({...form, otp: e.target.value})}/>
+        <button onClick={handleSubmit}>Register</button>
+      </form>
+
+      <ul>
+        <h3>Registered</h3>
+        {users.map((user, index) => (
+            <li key={index}>
+              {user.name} - {user.email}
+            </li>
+          ))}
+      </ul>
+
+     
+    </>
+  )
+}
+
+export default App
